83247647f4b0228a2e8a88334e7c3411d9a245cb
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-10-31 14:29:57 -0300

    feat: update toastfy

--- src/scripts/toastfy.js
+++ src/scripts/toastfy.js
@@ -1,9 +1,14 @@
-export const createToast = (content) => {
+export const createToast = (content, isOk) => {
     const body = document.querySelector('body')
     const div = document.createElement('div')
     const h4 = document.createElement('h4')
 
-    div.className = 'toast-error'
+    div.className = 'toast'
+    if (isOk) {
+        div.classList.add('toast-ok')
+    } else {
+        div.classList.add('toast-error')
+    }
     h4.innerText = content
     body.appendChild(div)
     div.appendChild(h4)
